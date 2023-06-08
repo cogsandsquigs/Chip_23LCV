c2d8153e9c2c6e11b90814f477a6655e453637e2
Ian Pratt <ianjdpratt@gmail.com>
2023-06-08 15:41:59 -0500

    checkpoint: got it working! now to do 2 chips...

--- src/Chip_23LCV.cpp
+++ src/Chip_23LCV.cpp
@@ -5,171 +5,316 @@
 
 Chip_23LCV::Chip_23LCV() {}
 
-void Chip_23LCV::begin(pin_t chips1)
+void Chip_23LCV::begin(pin_t chip1)
 {
-    this->chips = new pin_t[1]{chips1};
-    chip_count = 1;
-    pinMode(chips1, OUTPUT);
-    digitalWrite(chips1, HIGH);
+	uint32_t chip_size;
+	uint32_t addr_size;
+	detect_chip_size(chip1, &chip_size, &addr_size);
+
+	this->chips = new Chip[1]{Chip(chip1, chip_size, addr_size)};
+	chip_count = 1;
+
+	pinMode(chip1, OUTPUT);
+
+	digitalWrite(chip1, HIGH);
 }
 
-void Chip_23LCV::begin(pin_t chips1, pin_t chips2)
+void Chip_23LCV::begin(pin_t chip1, pin_t chip2)
 {
-    this->chips = new pin_t[2]{chips1, chips2};
-    chip_count = 2;
-    pinMode(chips1, OUTPUT);
-    pinMode(chips2, OUTPUT);
-    digitalWrite(chips1, HIGH);
-    digitalWrite(chips2, HIGH);
+	uint32_t chip1_size;
+	uint32_t addr1_size;
+	detect_chip_size(chip1, &chip1_size, &addr1_size);
+
+	uint32_t chip2_size;
+	uint32_t addr2_size;
+	detect_chip_size(chip2, &chip2_size, &addr2_size);
+
+	this->chips = new Chip[2]{Chip(chip1, chip1_size, addr1_size),
+							  Chip(chip2, chip2_size, addr2_size)};
+	chip_count = 2;
+
+	pinMode(chip1, OUTPUT);
+	pinMode(chip2, OUTPUT);
+
+	digitalWrite(chip1, HIGH);
+	digitalWrite(chip2, HIGH);
 }
 
 void Chip_23LCV::write(uint32_t address, byte *data, int length)
 {
-    /**
-     * Write to the specified address. If the data is longer than a single
-     * 23LCV1024 chip, write to the next chip in `chips`. If the address is
-     * greater than the end of the chip, write to the next chip in `chips`.
-     */
+	/**
+	 * Write to the specified address. If the data is longer than a single
+	 * 23LCV1024 chip, write to the next chip in `chips`. If the address is
+	 * greater than the end of the chip, write to the next chip in `chips`.
+	 */
 
-    // Calculate the starting chip index.
-    uint32_t chip = address / CHIP_SIZE;
-    uint32_t remaining_bytes = length;
+	// Calculate the starting chip index.
+	uint32_t chip = get_chip_index(address);
+	uint32_t remaining_bytes = length;
 
-    // Calculate how many chips we need to write to.
-    uint32_t chip_count = ((address % CHIP_SIZE) + length) / CHIP_SIZE + 1;
-    if (chip_count > chip_count)
-    {
-        chip_count = chip_count;
-    }
+	// Calculate how many chips we need to write to.
+	uint32_t chip_count = ((address % CHIP_SIZE) + length) / CHIP_SIZE + 1;
 
-    // Write to each chip.
-    for (uint32_t i = chip; i < (chip + chip_count); i++)
-    {
-        // Calculate the address to write to.
-        uint32_t chip_address = address % CHIP_SIZE;
+	// Write to each chip.
+	for (uint32_t i = chip; i < (chip + chip_count); i++)
+	{
+		// Calculate the address to write to.
+		uint32_t chip_address = address % CHIP_SIZE;
 
-        // Calculate the data to write.
-        uint32_t data_start = length - remaining_bytes;
-        uint32_t data_length = min((CHIP_SIZE - chip_address), remaining_bytes);
+		// Calculate the data to write.
+		uint32_t data_start = length - remaining_bytes;
+		uint32_t data_length = min((CHIP_SIZE - chip_address), remaining_bytes);
 
-        // Collect the data.
-        byte chip_data[data_length];
+		// Collect the data.
+		byte chip_data[data_length];
 
-        for (uint32_t j = 0; j < data_length; j++)
-        {
-            chip_data[j] = data[data_start + j];
-        }
+		for (uint32_t j = 0; j < data_length; j++)
+		{
+			chip_data[j] = data[data_start + j];
+		}
 
-        // Write to the chip.
-        write_single_chip(i, chip_address, chip_data, data_length);
+		// Write to the chip.
+		write_single_chip(i, chip_address, 4, chip_data, data_length);
 
-        remaining_bytes -= (CHIP_SIZE - chip_address);
-    }
+		remaining_bytes -= (CHIP_SIZE - chip_address);
+	}
 }
 
 void Chip_23LCV::write_byte(uint32_t address, byte data)
 {
-    // Manually doing this because this improves performance.
-    uint32_t chip = address / CHIP_SIZE;
-    uint32_t chip_address = address % CHIP_SIZE;
-    byte chip_data[1] = {data};
-    write_single_chip(chip, chip_address, chip_data, 1);
-}
+	// Manually doing this because this improves performance.
+	uint32_t chip = get_chip_index(address);
+	uint32_t chip_address = address % this->chips[chip].chip_size;
 
-void Chip_23LCV::write_single_chip(uint32_t chip, uint32_t address, byte data[], uint length)
-{
-    byte writing[length + 4];
-
-    writing[0] = 0x02;                         // write command
-    writing[1] = (byte)(address >> 16) & 0xFF; // address high
-    writing[2] = (byte)(address >> 8) & 0xFF;  // address mid
-    writing[3] = (byte)(address)&0xFF;         // address low
-    for (uint i = 0; i < length; i++)
-    {
-        writing[i + 4] = data[i];
-    }
-
-    SPI.begin(chips[chip]);
-    SPI.beginTransaction();
-    SPI.setDataMode(SPI_MODE0);
-    digitalWrite(chips[chip], LOW);
-    SPI.transfer(writing, NULL, length + 4, nullptr);
-    digitalWrite(chips[chip], HIGH);
-    SPI.endTransaction();
-    SPI.end();
+	byte chip_data[1] = {data};
+	write_single_chip(this->chips[chip].pin, chip_address,
+					  this->chips[chip].address_size, chip_data, 1);
 }
 
 void Chip_23LCV::read(uint32_t address, byte *(&data), int length)
 {
-    /**
-     * Read from the specified address. If the data is longer than a single
-     * 23LCV1024 chip, read from the next chip in `chips`. If the address is
-     * greater than the end of the chip, read from the next chip in `chips`.
-     */
+	/**
+	 * Read from the specified address. If the data is longer than a single
+	 * 23LCV1024 chip, read from the next chip in `chips`. If the address is
+	 * greater than the end of the chip, read from the next chip in `chips`.
+	 */
 
-    // Calculate the starting chip index.
-    uint32_t chip = address / CHIP_SIZE;
-    uint32_t remaining_bytes = length;
+	// Calculate the starting chip index.
+	uint32_t chip = get_chip_index(address);
+	uint32_t remaining_bytes = length;
 
-    // Calculate how many chips we need to read from.
-    uint32_t chip_count = ((address % CHIP_SIZE) + length) / CHIP_SIZE + 1;
-    if (chip_count > chip_count)
-    {
-        chip_count = chip_count;
-    }
+	// Calculate how many chips we need to read from.
+	uint32_t chip_count = get_chip_count(address, chip, length);
 
-    byte read[length];
+	byte read[length];
 
-    // Read from each chip.
-    for (uint32_t i = chip; i < (chip + chip_count); i++)
-    {
-        // Calculate the address to read from.
-        uint32_t chip_address = address % CHIP_SIZE;
+	// Read from each chip.
+	for (uint32_t i = chip; i < (chip + chip_count); i++)
+	{
+		// Get the size of the chip.
+		uint32_t chip_size = this->chips[chip].chip_size;
+		// Calculate the address to read from.
+		uint32_t chip_address = address % chip_size;
 
-        // Calculate the data to read.
-        uint32_t data_start = length - remaining_bytes;
-        uint32_t data_length = min((CHIP_SIZE - chip_address), remaining_bytes);
+		// Calculate the data to read.
+		uint32_t data_start = length - remaining_bytes;
+		uint32_t data_length = min((chip_size - chip_address), remaining_bytes);
 
-        // Read from the chip.
-        read_single_chip(i, chip_address, read, data_length);
+		// Read from the chip.
+		read_single_chip(this->chips[i].pin, chip_address, this->chips[i].address_size, read, data_length);
 
-        // Read data into the buffer.
-        for (uint32_t j = 0; j < data_length; j++)
-        {
-            data[data_start + j] = read[j];
-        }
+		// Read data into the buffer.
+		for (uint32_t j = 0; j < data_length; j++)
+		{
+			data[data_start + j] = read[j];
+		}
 
-        remaining_bytes -= (CHIP_SIZE - chip_address);
-    }
+		remaining_bytes -= (chip_size - chip_address);
+	}
 }
 
 byte Chip_23LCV::read_byte(uint32_t address)
 {
-    uint32_t chip = address / CHIP_SIZE;
-    uint32_t chip_address = address % CHIP_SIZE;
-    byte data[1];
+	uint32_t chip = get_chip_index(address);
+	uint32_t chip_address = address % this->chips[chip].chip_size;
+	byte data[1];
 
-    read_single_chip(chip, chip_address, data, 1);
-    return data[0];
+	read_single_chip(chip, chip_address, this->chips[chip].address_size, data, 1);
+	return data[0];
 }
 
-void Chip_23LCV::read_single_chip(uint32_t chip, uint32_t address, byte (&data)[], uint length)
+/* Private API */
+
+void Chip_23LCV::detect_chip_size(pin_t pin, uint32_t *chip_size,
+								  uint32_t *address_size)
 {
-    SPI.begin(chips[chip]);
-    SPI.setDataMode(SPI_MODE0);
+	uint32_t test_addr = 0x00FF;
+	byte test_data[1] = {(byte)random()};
 
-    digitalWrite(chips[chip], LOW);
+	// Check to see if it's a 23LCV1024 chip. If we can write to it with 4 address
+	// bytes, then it's a 23LCV1024 chip. Otherwise, it's a 23LCV512 chip. Write
+	// to the chip.
+	write_single_chip(pin, test_addr, 3, test_data, 1);
 
-    SPI.transfer(0x03); // Read command
+	// Read the data from the chip, at 0x0000.
+	byte data[2];
+	read_single_chip(pin, 0x0000, 2, data, 2);
 
-    // add the address to the data
-    SPI.transfer((address >> 16) & 0xFF);
-    SPI.transfer((address >> 8) & 0xFF);
-    SPI.transfer(address & 0xFF);
+	// Check if the data is correct.
+	// It's a 23LCV512 chip if we read the first byte as the last byte of the
+	// address and the second byte as the test data.
+	if (data[0] == 0xFF && data[1] == test_data[0])
+	{
+		*chip_size = 0x0FFFF;
+		*address_size = 2;
+	}
+	// It's a 23LCV1024 chip.
+	else
+	{
+		*chip_size = 0x1FFFF;
+		*address_size = 3;
+	}
+}
 
-    SPI.transfer(NULL, data, length, nullptr);
+uint32_t Chip_23LCV::get_chip_index(uint32_t address)
+{
+	// The total amount of memory we have looked through.
+	uint32_t total_memory = 0;
 
-    digitalWrite(chips[chip], HIGH);
+	for (uint32_t i = 0; i < chip_count; i++)
+	{
+		// Add the chip size to the total memory, so we can check if the address is
+		// greater than the total memory so far.
+		total_memory += chips[i].chip_size;
 
-    SPI.end();
+		// If we have found a chip where the address is less than the total memory,
+		// then we have found the chip we want.
+		if (address < chips[i].chip_size)
+		{
+			return i;
+		}
+
+		// If the address is greater than the total memory, then we need to keep
+		// searching!
+	}
+
+	// If we have gotten here, then we have a problem.
+	// This should NEVER happen!!!
+	return -1;
+}
+
+uint32_t Chip_23LCV::get_chip_count(uint32_t address, uint32_t chip_index,
+									uint32_t length)
+{
+	// The total amount of memory that we have searched through so far.
+	uint32_t total_memory = 0;
+
+	for (uint32_t i = chip_index; i < chip_count; i++)
+	{
+		// Add the chip size to the total memory, so we can check if the address is
+		// greater than the total memory so far.
+		total_memory += chips[i].chip_size;
+
+		// If this is the first chip, subtract the address from the total memory because we
+		// don't want to count memory before the address.
+		if (i == chip_index)
+		{
+			total_memory -= address;
+		}
+
+		// If the length is greater than the total memory, then we need to keep
+		// searching!
+		if (length > total_memory)
+		{
+			length -= total_memory;
+			continue;
+		}
+		// Otherwise, we have found the chip we want.
+		else
+		{
+			return i - chip_index + 1; // Inclusive of the current chip.
+		}
+
+		// If the address is greater than the total memory, then we need to keep
+		// searching!
+	}
+
+	// If we have gotten here, then we have a problem.
+	// This should NEVER happen!!!
+	return -1;
+}
+
+void Chip_23LCV::write_single_chip(pin_t pin, uint32_t address,
+								   uint32_t address_size, byte data[],
+								   uint length)
+{
+
+	byte writing[length + address_size + 1];
+
+	writing[0] = 0x02; // write command
+	// Write the address to the data
+	for (uint i = 0; i < address_size; i++)
+	{
+		// 8 * (address_size - i - 1) is the number of bits
+		// to shift, so that we can get the most significant
+		// 8 bits, then the next 8 bits, etc.
+		writing[i + 1] = (address >> (8 * (address_size - i - 1))) & 0xFF;
+	}
+
+	// Add the data to write.
+	for (uint i = 0; i < length; i++)
+	{
+		// +1 for the command byte.
+		writing[i + address_size + 1] = data[i];
+	}
+
+	Serial.print("Writing to chip: ");
+	for (uint i = 0; i < length + address_size + 1; i++)
+	{
+		Serial.print(writing[i], HEX);
+		Serial.print(" ");
+	}
+	Serial.println("");
+
+	SPI.begin(pin);
+	SPI.beginTransaction();
+	SPI.setDataMode(SPI_MODE0);
+
+	digitalWrite(pin, LOW);
+
+	SPI.transfer(writing, NULL, length + address_size + 1, nullptr);
+
+	digitalWrite(pin, HIGH);
+
+	SPI.endTransaction();
+	SPI.end();
+}
+
+void Chip_23LCV::read_single_chip(pin_t pin, uint32_t address,
+								  uint32_t address_size, byte (&data)[],
+								  uint length)
+{
+	SPI.begin(pin);
+	SPI.setDataMode(SPI_MODE0);
+
+	digitalWrite(pin, LOW);
+
+	SPI.transfer(0x03); // Read command
+
+	// Write the address to the data
+	for (uint32_t i = 0; i < address_size; i++)
+	{
+		// 8 * (address_size - i - 1) is the number of bits
+		// to shift, so that we can get the most significant
+		// 8 bits, then the next 8 bits, etc.
+		// +1 for the command byte.
+		SPI.transfer((address >> (8 * (address_size - i - 1))) & 0xFF);
+	}
+
+	// Read the incoming data from the chip.
+	SPI.transfer(NULL, data, length, nullptr);
+
+	digitalWrite(pin, HIGH);
+
+	SPI.end();
 }
--- src/Chip_23LCV.h
+++ src/Chip_23LCV.h
@@ -3,85 +3,158 @@
 class Chip_23LCV
 {
 public:
-    /**
-     * @brief Constructs an interface to the 23LCV1024 chip.
-     */
-    Chip_23LCV();
+	/**
+	 * @brief Constructs an interface to the 23LCV1024 chip.
+	 */
+	Chip_23LCV();
 
-    /**
-     * @brief Initializes the chip interface and SPI
-     *
-     * @param chips1 The chip select pin to use for all of the memory addresses.
-     */
-    void begin(pin_t chips1);
+	/**
+	 * @brief Initializes the chip interface and SPI
+	 *
+	 * @param chip1 The chip select pin to use for all of the memory addresses.
+	 */
+	void begin(pin_t chip1);
 
-    /**
-     * @brief Initializes the chip interface and SPI
-     *
-     * @param chips1 The chip select pin to use for the lower half of the memory addresses.
-     * @param chips2 The chip select pin to use for the upper half of the memory addresses.
-     */
-    void begin(pin_t chips1, pin_t chips2);
+	/**
+	 * @brief Initializes the chip interface and SPI
+	 *
+	 * @param chip1 The chip select pin to use for the lower half of the memory
+	 * addresses.
+	 * @param chip2 The chip select pin to use for the upper half of the memory
+	 * addresses.
+	 */
+	void begin(pin_t chip1, pin_t chip2);
 
-    /**
-     * @brief Writes bytes to the specified address.
-     *
-     * @param address The address to write to.
-     * @param data The data to write.
-     * @param length The number of bytes to write.
-     */
-    void write(uint32_t address, byte data[], int length);
+	/**
+	 * @brief Writes bytes to the specified address.
+	 *
+	 * @param address The address to write to.
+	 * @param data The data to write.
+	 * @param length The number of bytes to write.
+	 */
+	void write(uint32_t address, byte data[], int length);
 
-    /**
-     * @brief Writes a byte to the specified address.
-     *
-     * @param address The address to read from.
-     * @param data The buffer to store the data in.
-     */
-    void write_byte(uint32_t address, byte data);
+	/**
+	 * @brief Writes a byte to the specified address.
+	 *
+	 * @param address The address to read from.
+	 * @param data The buffer to store the data in.
+	 */
+	void write_byte(uint32_t address, byte data);
 
-    /**
-     * @brief Reads bytes from the specified address.
-     *
-     * WARNING: If you do not delete() your data array
-     * afterwards, or don't make your data array
-     * static, you will leak memory.
-     *
-     * @param address The address to read from.
-     * @param data The buffer to read the data into.
-     * @param length The number of bytes to read.
-     */
-    void read(uint32_t address, byte *(&data), int length);
+	/**
+	 * @brief Reads bytes from the specified address.
+	 *
+	 * WARNING: If you do not delete() your data array
+	 * afterwards, or don't make your data array
+	 * static, you will leak memory.
+	 *
+	 * @param address The address to read from.
+	 * @param data The buffer to read the data into.
+	 * @param length The number of bytes to read.
+	 */
+	void read(uint32_t address, byte *(&data), int length);
 
-    /**
-     * @brief Reads a byte from the specified address.
-     *
-     * @param address The address to read from.
-     * @returns The byte read
-     */
-    byte read_byte(uint32_t address);
+	/**
+	 * @brief Reads a byte from the specified address.
+	 *
+	 * @param address The address to read from.
+	 * @returns The byte read
+	 */
+	byte read_byte(uint32_t address);
 
 private:
-    /**
-     * @brief Writes bytes to the specified chip and address.
-     *
-     * @param chip The index of the chip to write to.
-     * @param address The address to write to.
-     * @param data The data to write.
-     * @param length The number of bytes to write.
-     */
-    void write_single_chip(uint32_t chip, uint32_t address, byte data[], uint length);
+	/**
+	 * @brief Detect the type of chip that is being interfaced with
+	 *
+	 * @param pin The pin to use for the chip select.
+	 * @param chip_size The size of the chip in bytes. This is a pointer which will be set
+	 * to the correct chip size, as an output.
+	 * @param address_size The number of bytes in the address. This is a pointer which will
+	 * be set to the correct address size, as an output.
+	 */
+	void detect_chip_size(pin_t pin, uint32_t *chip_size, uint32_t *address_size);
 
-    /**
-     * @brief Reads bytes from the specified chip and address.
-     *
-     * @param chip The index of the chip to read from.
-     * @param address The address to read from.
-     * @param data The buffer to read the data into.
-     * @param length The number of bytes to read.
-     */
-    void read_single_chip(uint32_t chip, uint32_t address, byte (&data)[], uint length);
+	/**
+	 * @brief Gets the starting chip index for the specified address.
+	 *
+	 * @param address The address to get the chip index for.
+	 */
+	uint32_t get_chip_index(uint32_t address);
 
-    pin_t *chips;
-    uint32_t chip_count;
+	/**
+	 * @brief Calculates the amount of chips to read from for the specified address, chip index,
+	 * and length.
+	 *
+	 * @param address The address to read from.
+	 * @param chip_index The index of the chip to read from.
+	 * @param length The number of bytes to read.
+	 */
+	uint32_t get_chip_count(uint32_t address, uint32_t chip_index, uint32_t length);
+
+	/**
+	 * @brief Writes bytes to the specified chip and address.
+	 *
+	 * @param pin The pin of the chip to write to.
+	 * @param address The address to write to.
+	 * @param address_size The number of bytes in the address.
+	 * @param data The data to write.
+	 * @param length The number of bytes to write.
+	 */
+	void write_single_chip(pin_t pin, uint32_t address, uint32_t address_size, byte data[], uint length);
+
+	/**
+	 * @brief Reads bytes from the specified chip and address.
+	 *
+	 * @param pin The index of the chip to read from.
+	 * @param address The address to read from.
+	 * @param address_size The number of bytes in the address.
+	 * @param data The buffer to read the data into.
+	 * @param length The number of bytes to read.
+	 */
+	void read_single_chip(pin_t pin, uint32_t address, uint32_t address_size, byte (&data)[], uint length);
+
+	class Chip
+	{
+
+	public:
+		/**
+		 * @brief Constructs a new chip.
+		 *
+		 * @param chip_pin The chip select pin for this chip.
+		 * @param chip_size The size of the address space of this chip.
+		 * @param address_size The number of bytes in the address.
+		 */
+		Chip(pin_t chip_pin, uint32_t chip_size_, uint32_t address_size_)
+		{
+			pin = chip_pin;
+			chip_size = chip_size_;
+			address_size = address_size_;
+		};
+
+		/**
+		 * @brief The chip select pin for this chip.
+		 */
+		pin_t pin;
+
+		/**
+		 * @brief The number of bytes in the address.
+		 */
+		uint8_t address_size;
+
+		/**
+		 * @brief The size of the address space of this chip.
+		 */
+		uint32_t chip_size;
+	};
+
+	/**
+	 * @brief The list of chips to write to.
+	 */
+	Chip *chips;
+
+	/**
+	 * @brief The number of chips in `chips`.
+	 */
+	uint32_t chip_count;
 };
